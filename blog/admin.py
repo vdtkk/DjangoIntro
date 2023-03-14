fab9d97a2bff5d816388e01e45bc61b5cc545212
vdtkk <vdtkk@hotmail.com>
2023-03-14 19:26:47 +0300

    iletisim paneli ve admin ayarları yapıldı.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from blog.models import (KategoriModel 
-                         ,YazilarModel,YorumModel)
+                         ,YazilarModel,YorumModel,iletisimModel)
+
 
 
 admin.site.register(KategoriModel)
@@ -20,3 +21,9 @@ class YorumAdmin(admin.ModelAdmin):
 
 
 admin.site.register(YorumModel,YorumAdmin)
+
+class iletisimAdmin(admin.ModelAdmin):
+    list_display=('email','olusturma_Tarihi')
+    search_fields=('email',)
+
+admin.site.register(iletisimModel,iletisimAdmin)
