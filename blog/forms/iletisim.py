5b84db436116a7dd95119abacb38af5fb960a4bc
vdtkk <vdtkk@hotmail.com>
2023-04-13 14:44:39 +0300

    model formlar

--- blog/forms/iletisim.py
+++ blog/forms/iletisim.py
@@ -1,6 +1,13 @@
 from django import forms
+from blog.models import iletisimModel
 
-class Iletisimform(forms.Form):
-        email= forms.EmailField(label='E-Posta',max_length=100)
-        isim_soyisim = forms.CharField(label='İsim Soyisim', max_length=10)
-        mesaj=forms.CharField(label='Mesajınız',widget=forms.Textarea)
+class Iletisimform(forms.ModelForm):
+        class Meta:
+           model= iletisimModel
+           fields=('isim_soyisim','email','mesaj')
+
+
+# class Iletisimform(forms.Form):
+#         email= forms.EmailField(label='E-Posta',max_length=100)
+#         isim_soyisim = forms.CharField(label='İsim Soyisim', max_length=10)
+#         mesaj=forms.CharField(label='Mesajınız',widget=forms.Textarea)
--- blog/views/iletisim.py
+++ blog/views/iletisim.py
@@ -12,11 +12,12 @@ def iletisim(request):
         if form.is_valid():
             # print(form.cleaned_data.get('email'))
 
-           iletisim=iletisimModel()
-           iletisim.email=form.cleaned_data['email']
-           iletisim.isim_soyisim = form.cleaned_data['isim_soyisim']
-           iletisim.mesaj=form.cleaned_data['mesaj']
-           iletisim.save() 
+        #    iletisim=iletisimModel()
+        #    iletisim.email=form.cleaned_data['email']
+        #    iletisim.isim_soyisim = form.cleaned_data['isim_soyisim']
+        #    iletisim.mesaj=form.cleaned_data['mesaj']
+        #    iletisim.save() 
+           form.save()
            return redirect('anasayfa')
 
         else:
