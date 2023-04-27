46a2ac5312a94299c72cc390a10c14d8d1d6d02b
vdtkk <vdtkk@hotmail.com>
2023-04-27 11:34:24 +0300

    son hali

--- blog/views/yazi_guncelle.py
+++ blog/views/yazi_guncelle.py
@@ -7,5 +7,10 @@ def yazi_guncelle(request,slug):
     yazi=get_object_or_404(YazilarModel, slug=slug, yazar=request.user)
     form=YaziGuncelleFormModel(request.POST or None, files=request.FILES or None, instance=yazi)
     if form.is_valid():
+        form.save()
         return redirect('detay', slug=yazi.slug)    
-    return render(request , 'pages/yazi-guncelle.html', context={ })
+    return render(request , 'pages/yazi-guncelle.html', context={ 
+        'form':form
+
+        
+    })
