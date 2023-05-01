d9ccb20efcf0146d53bc3ec30cffabf5fdf84b2e
vdtkk <vdtkk@hotmail.com>
2023-05-01 18:05:34 +0300

    yazi silme işlemleri eklendi

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,24 @@
-
-
 from django.urls import path, include
-from blog.views import iletisim, anasayfa,kategori,yazilarim,detay,yazi_ekle,yazi_guncelle
-#from django.contrib.auth import view as aut_views  
-urlpatterns = [
-    #path('giris', aut_views.LoginView.as_view(template_name='pages/giris.html' ), name='giris'),
-    path('', anasayfa, name='anasayfa'),
-    path('iletisim', iletisim,name='iletisim'),
-    path('kategori/<slug:kategoriSlug>', kategori, name='kategori'),
-    path('yazilarim', yazilarim, name='yazilarim'),
-    path('detay/<slug:slug>', detay, name='detay'),
-     path('yazi-ekle', yazi_ekle, name='yazi'),
-     path('yazi-guncelle/<slug:slug>', yazi_guncelle, name='yazi-guncelle'),
-    ]
+from blog.views import (
+    iletisim,
+    anasayfa,
+    kategori,
+    yazilarim,
+    detay,
+    yazi_ekle,
+    yazi_guncelle,
+    yazi_sil,
+)
 
+# from django.contrib.auth import view as aut_views
+urlpatterns = [
+    # path('giris', aut_views.LoginView.as_view(template_name='pages/giris.html' ), name='giris'),
+    path("", anasayfa, name="anasayfa"),
+    path("iletisim", iletisim, name="iletisim"),
+    path("kategori/<slug:kategoriSlug>", kategori, name="kategori"),
+    path("yazilarim", yazilarim, name="yazilarim"),
+    path("detay/<slug:slug>", detay, name="detay"),
+    path("yazi-sil/<slug:slug>", yazi_sil, name="yazi-sil"),
+    path("yazi-ekle", yazi_ekle, name="yazi"),
+    path("yazi-guncelle/<slug:slug>", yazi_guncelle, name="yazi-guncelle"),
+]
--- blog/views/yazilarim.py
+++ blog/views/yazilarim.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='/')
+
+@login_required(login_url="/")
 def yazilarim(request):
+    yazilar = request.user.yazilar.order_by("-id")
+    sayfa = request.GET.get("sayfa")
+    paginator = Paginator(yazilar, 5)
 
-   yazilar = request.user.yazilar.order_by('-id')
-   sayfa=request.GET.get('sayfa')
-   paginator = Paginator(yazilar,2)
-
-   
-   return render(request, 'pages/yazilarim.html',context={
-      'yazilar':paginator.get_page(sayfa),
-      
-   })
+    return render(
+        request,
+        "pages/yazilarim.html",
+        context={
+            "yazilar": paginator.get_page(sayfa),
+        },
+    )
